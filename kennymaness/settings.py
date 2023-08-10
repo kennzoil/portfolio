61eb56c8f48ba2fcfeae8eb0cd005424e5344e14
kennzoil <lewisfromsaintlouis@gmail.com>
2023-08-10 15:33:30 -0500

    bug fixes and performance improvements

--- kennymaness/settings.py
+++ kennymaness/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,6 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
+load_dotenv(".env")
+
 try:
     SECRET_KEY = os.environ["SECRET_KEY"]
 except KeyError as e:
@@ -30,8 +34,9 @@ except KeyError as e:
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    ".kennymaness.com",
-    "localhost",
+    #"kennymaness.com",
+    #"localhost",
+    "*",
     ]
 
 
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('portfolio/', views.portfolio, name='portfolio'),
+    path('', views.portfolio, name='portfolio'),
 ]
