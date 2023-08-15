8607128b3f855b9dc2d8126d44cb3418b4e7eaf5
Kenny Maness <lewisfromsaintlouis@gmail.com>
2023-08-15 17:12:17 -0500

    add static files

--- kennymaness/settings.py
+++ kennymaness/settings.py
@@ -128,7 +128,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'kennymaness/static/'
+
+STATICFILES_DIR = [BASE_DIR / "kennymaness/static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
