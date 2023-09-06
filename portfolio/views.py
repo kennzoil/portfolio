2c09c0454fb827e0a9e6dac74e2e2ecc54af562e
Kenny Maness <lewisfromsaintlouis@gmail.com>
2023-09-05 23:38:50 -0500

    cool landing page

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.template import loader
 
 def portfolio(request):
-    template = loader.get_template('homepage.html')
+    template = loader.get_template('under_construction.html')
     return HttpResponse(template.render())
