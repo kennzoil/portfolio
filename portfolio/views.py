7991ffcd41e6609c6d0e3b4a31674ff9665e7da9
Kenny Maness <lewisfromsaintlouis@gmail.com>
2023-09-13 15:35:54 -0500

    create 'about' page

--- portfolio/views.py
+++ portfolio/views.py
@@ -4,3 +4,7 @@ from django.template import loader
 def portfolio(request):
     template = loader.get_template('homepage.html')
     return HttpResponse(template.render())
+
+def about(request):
+    # template = loader.get_template('about.html')
+    return HttpResponse(loader.get_template('about.html').render())
